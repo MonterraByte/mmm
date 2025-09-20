8bc57da9e59fb89ac02604e786791922730ba1fc
Joaquim Monteiro <joaquim.monteiro@protonmail.com>
2025-09-20 04:43:25 +0100

    Implement overlayfs mounting

--- src/mount.rs
+++ src/mount.rs
@@ -14,17 +14,34 @@
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 use std::io;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 use rustix::fs::CWD;
 use rustix::io::Errno;
 use rustix::mount::{
-    FsMountFlags, FsOpenFlags, MountAttrFlags, MoveMountFlags, UnmountFlags, fsconfig_create, fsconfig_set_string,
-    fsmount, fsopen, move_mount, unmount,
+    FsMountFlags, FsOpenFlags, MountAttrFlags, MoveMountFlags, UnmountFlags, fsconfig_create, fsconfig_set_path,
+    fsconfig_set_string, fsmount, fsopen, move_mount, unmount,
 };
 use rustix::process::{getgid, getuid};
 use tempfile::TempDir;
 use thiserror::Error;
+
+fn mount_overlayfs(staging_dir: &Path, game_dir: &Path) -> Result<(), MountError> {
+    let fs_fd = fsopen("overlay", FsOpenFlags::FSOPEN_CLOEXEC).map_err(MountError::FsOpen)?;
+    fsconfig_set_string(&fs_fd, "source", "overlay").map_err(MountError::FsConfigSet)?;
+    fsconfig_set_path(&fs_fd, "lowerdir+", staging_dir, CWD).map_err(MountError::FsConfigSet)?;
+    fsconfig_set_path(&fs_fd, "lowerdir+", game_dir, CWD).map_err(MountError::FsConfigSet)?;
+    fsconfig_create(&fs_fd).map_err(MountError::FsConfigCreate)?;
+
+    let mfd = fsmount(
+        &fs_fd,
+        FsMountFlags::FSMOUNT_CLOEXEC,
+        MountAttrFlags::MOUNT_ATTR_NODEV | MountAttrFlags::MOUNT_ATTR_NOSUID | MountAttrFlags::MOUNT_ATTR_NOATIME,
+    )
+    .map_err(MountError::FsMount)?;
+
+    move_mount(mfd, "", CWD, game_dir, MoveMountFlags::MOVE_MOUNT_F_EMPTY_PATH).map_err(MountError::MoveMount)
+}
 
 fn mount_tmpfs(dir: &Path) -> Result<(), MountError> {
     let uid = getuid();
@@ -61,40 +78,41 @@ pub enum MountError {
     MoveMount(#[source] Errno),
 }
 
-pub struct TempMount(Option<TempDir>);
+#[derive(Debug)]
+pub struct OverlayMount(UnmountWrapper<PathBuf>);
+
+impl OverlayMount {
+    pub fn new(staging_dir: &Path, game_dir: &Path) -> Result<Self, MountError> {
+        mount_overlayfs(staging_dir, game_dir)?;
+        Ok(Self(UnmountWrapper::new(game_dir.to_owned())))
+    }
+
+    pub fn path(&self) -> &Path {
+        self.0.path()
+    }
+
+    pub fn unmount(self) -> Result<(), Errno> {
+        self.0.unmount().and(Ok(()))
+    }
+}
+
+#[derive(Debug)]
+pub struct TempMount(UnmountWrapper<TempDir>);
 
 impl TempMount {
     pub fn new() -> Result<Self, TempMountCreationError> {
         let temp_dir = TempDir::with_prefix("mmm-").map_err(TempMountCreationError::TempDir)?;
         mount_tmpfs(temp_dir.path())?;
-        Ok(Self(Some(temp_dir)))
+        Ok(Self(UnmountWrapper::new(temp_dir)))
     }
 
     pub fn path(&self) -> &Path {
-        self.0.as_ref().expect("not dropped yet").path()
+        self.0.path()
     }
 
-    pub fn unmount(mut self) -> Result<(), TempMountUnmountError> {
-        self.unmount_inner().map_err(TempMountUnmountError::Unmount)?;
-        if let Some(path) = self.0.take() {
-            path.close().map_err(TempMountUnmountError::TempDir)?;
-        }
-
-        Ok(())
-    }
-
-    fn unmount_inner(&mut self) -> Result<(), Errno> {
-        unmount(self.path(), UnmountFlags::DETACH | UnmountFlags::NOFOLLOW)
-    }
-}
-
-impl Drop for TempMount {
-    fn drop(&mut self) {
-        if self.0.is_none() {
-            // already unmounted
-            return;
-        }
-        let _ = self.unmount_inner();
+    pub fn unmount(self) -> Result<(), TempMountUnmountError> {
+        let temp_dir = self.0.unmount().map_err(TempMountUnmountError::Unmount)?;
+        temp_dir.close().map_err(TempMountUnmountError::TempDir)
     }
 }
 
@@ -112,4 +130,36 @@ pub enum TempMountUnmountError {
     TempDir(#[source] io::Error),
     #[error("failed to unmount tmpfs: {0}")]
     Unmount(#[source] Errno),
+}
+
+#[derive(Debug)]
+struct UnmountWrapper<P: AsRef<Path>>(Option<P>);
+
+impl<P: AsRef<Path>> UnmountWrapper<P> {
+    pub fn new(path: P) -> Self {
+        Self(Some(path))
+    }
+
+    pub fn path(&self) -> &Path {
+        self.0.as_ref().expect("not dropped yet").as_ref()
+    }
+
+    pub fn unmount(mut self) -> Result<P, Errno> {
+        self.unmount_inner()?;
+        Ok(self.0.take().expect("not dropped yet"))
+    }
+
+    fn unmount_inner(&self) -> Result<(), Errno> {
+        unmount(self.path(), UnmountFlags::DETACH | UnmountFlags::NOFOLLOW)
+    }
+}
+
+impl<P: AsRef<Path>> Drop for UnmountWrapper<P> {
+    fn drop(&mut self) {
+        if self.0.is_none() {
+            // already unmounted
+            return;
+        }
+        let _ = self.unmount_inner();
+    }
 }
