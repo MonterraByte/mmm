1739856377972f564e0df72d49b2573c205ac635
Joaquim Monteiro <joaquim.monteiro@protonmail.com>
2026-02-05 09:43:21 +0000

    Move Instance::save call to update function

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -82,6 +82,8 @@ impl App for ModManagerUi {
         CentralPanel::default().show(ctx, |ui| {
             self.center_panel(ui);
         });
+
+        self.instance.save();
     }
 }
 
@@ -256,8 +258,6 @@ impl ModManagerUi {
             self.selection
                 .extend(drop_index.inclusive_range_to(drop_index.saturating_add(selection_len).saturating_sub(1u32)));
         }
-
-        self.instance.save();
     }
 
     fn create_empty_mod_modal(&mut self, ui: &mut Ui) {
